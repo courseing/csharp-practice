4874282f4725527bbdcb4e0d272c2945b44896c9
Khalil Kothia <khalilckothia@gmail.com>
2018-11-10 12:23:16 +0530

    1st time upload
    1st time upload

---
+++ 09Nov2018/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09Nov2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DataTypes(); 
+            //IfConditions();
+            //SwitchConditions();
+            //Looping();
+            Arrays();
+        }
+
+        private static void Arrays()
+        {
+            string[,] StudentsCourses = new string[5,2];
+            string StudentName = "Kumar";
+
+
+            StudentName = StudentName.(50-StudentName.Length);
+
+            DateTime dt = new DateTime(2018, 11, 10);
+
+
+
+            StudentsCourses[0,0] = "";
+            StudentsCourses[0, 1] = "";
+
+            string[][] PatientInvestigations = new string[3][];
+            PatientInvestigations[0] = new string[3];
+
+
+
+            int[] PlayersScore = new int[5];
+            PlayersScore[0] = 11;
+            PlayersScore[1] = 12;
+            PlayersScore[2] = 13;
+            PlayersScore[3] = 14;
+            PlayersScore[4] = 15;
+
+
+            foreach (int PlayerScore in PlayersScore)
+            {
+                Console.WriteLine(PlayerScore);
+            }
+            Console.ReadLine();
+
+            for (int PlayerNo=0; PlayerNo<PlayersScore.Length; PlayerNo++)
+            {
+                Console.WriteLine(PlayersScore[PlayerNo]);
+            }
+            Console.ReadLine();
+
+
+            //int Player1Score = 20;
+            //Player1Score.
+
+        }
+
+        private static void Looping()
+        {
+            //for, while, do while
+
+            int counter;
+            for(counter=1;counter<=10;counter++)
+            {
+                Console.WriteLine("Number " + counter);
+            }
+
+            counter = 1;
+            for (; counter <= 10; counter++)
+            {
+                Console.WriteLine("Number " + counter);
+            }
+
+            counter = 1;
+            for (; counter <= 10; )
+            {
+                if (counter == 7) break;
+                
+                counter++;
+                if (counter == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine("Number " + counter);
+                Console.WriteLine("Number " + counter);
+                Console.WriteLine("Number " + counter);
+                Console.WriteLine("Number " + counter);
+            }
+            // 1,2,3,4,5,6
+            // 1,2,3,4,5,
+
+
+
+            // 2,3,4,6
+
+            counter = 1;
+            while (counter <= 10)
+            {
+                Console.WriteLine("Number " + counter);
+                counter++;
+            }
+
+
+            counter = 1;
+            do
+            {
+                Console.WriteLine("Number " + counter);
+                counter++;
+            }
+            while (counter <= 10);
+
+        }
+
+        static void SwitchConditions()
+        {
+            int num1;
+
+            Console.WriteLine("Plesae enter number 2");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            switch(num1)
+            {
+                case 1:
+                    Console.WriteLine("Number  " + num1);
+                    break;
+                case 2:
+                    Console.WriteLine("Number  " + num1);
+                    break;
+                default:
+                    Console.WriteLine("Number  " + num1);
+                    break;
+            }
+
+            //switch(z)    
+            //case 1
+            //True Block
+            //case 2
+            //True Block
+            //default
+
+        }
+
+        static void DataTypes()
+        {
+            int num1, num2;
+            string fname = "K";
+            string lname = "Gandhi";
+            string name = fname + lname;
+
+            Console.WriteLine("Plesae enter number 2");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Plesae enter number 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum of entered numbers is " + (num1 + num2));
+            Console.Read();
+        }
+        static void IfConditions()
+        {
+            int num1, num2;
+
+            Console.WriteLine("Plesae enter number 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            //   T    ||   T >>> T
+            if (num1 <= 0 || num1 >= 100)  //num1 = 10
+                Console.WriteLine("Number is not valid");
+
+            //   T    ||   T >>> T
+            if (num1 >= 0 && num1 <= 100)  //num1 = 10
+                Console.WriteLine("Number is not valid");
+
+
+            if (num1 <= 0)  //num1 = 10
+                Console.WriteLine("Number is not valid");
+            else if (num1 >= 100)
+                Console.WriteLine("Number is not valid");
+            else
+            {
+
+            }
+
+            if (num1 <= 0)
+            {
+                Console.WriteLine(num1 + " is less than zero. Cannot continue");
+                return;
+            }
+
+
+            if (num1 <= 0)
+            {
+                Console.WriteLine(num1 + " is less than zero. Cannot continue");
+                return;
+            }
+            else
+            {
+                ///
+            }
+        }
+    }
+}
+
---
+++ 10Nov2018/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10Nov2018
+{
+    class Program
+    {
+        static String P_name1 = "Rakesh", P_name2 = "Ramesh", P_name3 = "Kartik", P_name4 = "Vinay ", P_name5 = "Raj   ", P_name6 = "Nikhil", P_name7 = "Vicky "; //Declaring all the patient name.
+        static int P_age1 = 22, P_age2 = 25, P_age3 = 27, P_age4 = 28, P_age5 = 30, P_age6 = 35, P_age7 = 20; //Declaring ages of all the patient.
+        static int P_id1 = 1, P_id2 = 2, P_id3 = 3, P_id4 = 4, P_id5 = 5, P_id6 = 6, P_id7 = 7; //Declaring unique ID's of Each patient.
+        static String P_disease1 = "Cold       ", P_disease2 = "Cough      ", P_disease3 = "HeartAttack", P_disease4 = "Cold       ", P_disease5 = "SkinDisease", P_disease6 = "Cough      ", P_disease7 = "HeartAttack";//declaring the various diseases
+        static string d_name1 = "Dr. A ", d_name2 = "Dr. B ", d_name3 = "Dr. C ";
+        static string d_specialization1 = "Cardiologist ", d_specialization2 = "Dermatologist", d_specialization3 = "Radiologist  ";
+        static string d_A1 = "A", d_A2 = "A", d_A3 = "A";
+        static int cost = 0;
+        static int choice1, choice2;
+        static int P_C_id;
+        static string doctor;
+        
+        static void Main(string[] args)
+        {
+
+            string Choice1Input = "";
+            do
+            {
+                Console.WriteLine("Welcome to APOLLO HOSPITAL. We are here to HELP YOU..!");
+                Console.WriteLine(" ");
+                Console.WriteLine("Please Enter Your Answer");
+                Console.WriteLine("Press 1 for Doctors Database");
+                Console.WriteLine("Press 2 for Patients Database");
+                Console.WriteLine("Press 3 to exit.");
+
+                Choice1Input = Console.ReadLine().ToString();
+                if (String.IsNullOrEmpty(Choice1Input))
+                {
+                    choice1 = 0;
+                }
+                else
+                {
+                    choice1 = Convert.ToInt32(Choice1Input);
+                }
+                
+                if (choice1 == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("------------------------------------------------------- Doctors Database ------------------------------------------");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Doctors Details");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Name                              Specialization                                       Availability");
+                    Console.WriteLine(" ");
+                    Console.WriteLine(d_name1 + "                           " + d_specialization1 + "                                          " + d_A1);
+                    Console.WriteLine(d_name2 + "                           " + d_specialization2 + "                                          " + d_A2);
+                    Console.WriteLine(d_name3 + "                           " + d_specialization3 + "                                          " + d_A3);
+                    //Console.Read();
+                }
+                else if (choice1 == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please select a option below");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Select 1 for Patients Database");
+                    Console.WriteLine("Select 2 for existing patient. ");
+                    Console.WriteLine("Select 3 for new patient. ");
+                    choice2 = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice2 == 2)
+                    {
+                        Console.WriteLine("What is your patient ID");
+                        P_C_id = Convert.ToInt32(Console.ReadLine());
+                        switch (P_C_id)
+                        {
+
+                            case 1:
+                                Console.WriteLine("Patient's Name            Patient's Age               Patient's Problem              Patient's ID");
+                                Console.WriteLine(" ");
+                                Console.WriteLine(P_name1 + "                     " + P_age1 + "                             " + P_disease1 + "                        " + P_id1);
+                                break;
+                            case 2:
+                                Console.WriteLine("Patient's Name            Patient's Age               Patient's Problem              Patient's ID");
+                                Console.WriteLine(" ");
+                                Console.WriteLine(P_name2 + "                     " + P_age2 + "                             " + P_disease2 + "                        " + P_id2);
+                                break;
+                            case 3:
+                                Console.WriteLine("Patient's Name            Patient's Age               Patient's Problem              Patient's ID");
+                                Console.WriteLine(" ");
+                                Console.WriteLine(P_name3 + "                     " + P_age3 + "                             " + P_disease3 + "                        " + P_id3);
+                                break;
+                            case 4:
+                                Console.WriteLine("Patient's Name            Patient's Age               Patient's Problem              Patient's ID");
+                                Console.WriteLine(" ");
+                                Console.WriteLine(P_name4 + "                     " + P_age4 + "                             " + P_disease4 + "                        " + P_id4);
+                                break;
+                            case 5:
+                                Console.WriteLine("Patient's Name            Patient's Age               Patient's Problem              Patient's ID");
+                                Console.WriteLine(" ");
+                                Console.WriteLine(P_name5 + "                     " + P_age5 + "                             " + P_disease5 + "                        " + P_id5);
+
+                                break;
+                            case 6:
+                                Console.WriteLine("Patient's Name            Patient's Age               Patient's Problem              Patient's ID");
+                                Console.WriteLine(" ");
+                                Console.WriteLine(P_name6 + "                     " + P_age6 + "                             " + P_disease6 + "                        " + P_id6);
+                                break;
+                            case 7:
+                                Console.WriteLine("Patient's Name            Patient's Age               Patient's Problem              Patient's ID");
+                                Console.WriteLine(" ");
+                                Console.WriteLine(P_name7 + "                     " + P_age7 + "                             " + P_disease7 + "                        " + P_id7);
+                                break;
+                            default:
+                                Console.WriteLine("record not present");
+                                break;
+
+                        }
+                    }
+                    else if (choice2 == 1)
+                    {
+                        Console.WriteLine("welcome to patient Database.");
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Patient's Name            Patient's Age               Patient's Problem              Patient's ID");
+                        Console.WriteLine(" ");
+                        Console.WriteLine(P_name1 + "                     " + P_age1 + "                             " + P_disease1 + "                        " + P_id1);
+                        Console.WriteLine(P_name2 + "                     " + P_age2 + "                             " + P_disease2 + "                        " + P_id2);
+                        Console.WriteLine(P_name3 + "                     " + P_age3 + "                             " + P_disease3 + "                        " + P_id3);
+                        Console.WriteLine(P_name4 + "                     " + P_age4 + "                             " + P_disease4 + "                        " + P_id4);
+                        Console.WriteLine(P_name5 + "                     " + P_age5 + "                             " + P_disease5 + "                        " + P_id5);
+                        Console.WriteLine(P_name6 + "                     " + P_age6 + "                             " + P_disease6 + "                        " + P_id6);
+                        Console.WriteLine(P_name7 + "                     " + P_age7 + "                             " + P_disease7 + "                        " + P_id7);
+                    }
+                    else if (choice2 == 3)
+                    {
+                        Console.WriteLine("Enter the Patients Name");
+                        string Patient_1 = Console.ReadLine();
+                        Console.WriteLine("Enter patients age");
+                        int P_Age = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter date");
+                        string date = Console.ReadLine();
+                        Console.WriteLine("Enter Patients Problem");
+                        string Problem = Console.ReadLine();
+                        Console.WriteLine("Enter the Diagnosis Required");
+                        Console.WriteLine("1. X ray");
+                        Console.WriteLine("2. Blood Test");
+                        Console.WriteLine("3. ECG");
+
+                        int j = Convert.ToInt32(Console.ReadLine());
+                        switch (j)
+                        {
+                            case 1:
+                                cost = 1500;
+                                doctor = d_name1;
+                                break;
+                            case 2:
+                                cost = 1000;
+                                doctor = d_name2;
+                                break;
+                            case 3:
+                                cost = 1600;
+                                doctor = d_name3;
+                                break;
+                        }
+                        Console.Clear();
+                        Console.WriteLine("                         APOLLO HOSPITAL");
+                        Console.WriteLine("----------------------------------------------------------------------------");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        Console.WriteLine("Patient Name:" + Patient_1);
+                        Console.WriteLine("");
+                        Console.WriteLine("Age:   " + P_Age);
+                        Console.WriteLine("");
+                        Console.WriteLine("Consultation Date:" + date);
+                        Console.WriteLine("");
+                        Console.WriteLine("Doctors Name:" + d_name1);
+                        Console.WriteLine("");
+                        Console.WriteLine("cost     " + cost);
+                        Console.WriteLine("");
+
+
+                        string Procedure = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input");
+                    }
+                }
+            } while (choice1 != 3);
+        }
+    }
+}
